40f8fcbec9381855ded8399db640d8a35033e690
Minerva-Lan <lan2011gator@gmail.com>
2022-01-06 12:11:44 -0800

    Stochastic Gradient Descent

--- Stochastic Gradient Descent/linear_unit.py
+++ Stochastic Gradient Descent/linear_unit.py
@@ -1,4 +1,6 @@
-from Perceptron.perceptron import Perceptron
+import sys
+sys.path.insert(1, '../Deep-Learning-in-Action/Perceptron')
+from perceptron import Perceptron
 
 f = lambda x:x
 class LinearUnit(Perceptron):
@@ -9,3 +11,31 @@ def get_training_dataset():
     input_vecs =[[5], [3], [8], [1.4], [10.1]]
     labels =[5500, 2300, 7600, 1800, 11400]
     return input_vecs, labels
+
+def train_linear_unit():
+    lu = LinearUnit(1)
+    input_vecs, labels = get_training_dataset()
+    lu.train(input_vecs, labels, 10, 0.01)
+    return lu
+
+def plot(linear_unit):
+    import matplotlib.pyplot as plt
+    input_vecs, labels = get_training_dataset()
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(list(map(lambda x: x[0], input_vecs)), labels)
+    weights = linear_unit.weights
+    bias = linear_unit.bias
+    x = range(0, 12, 1)
+    y=list(map(lambda x:weights[0] * x + bias, x))
+    ax.plot(x, y)
+    plt.show()
+
+if __name__=='__main__':
+    linear_unit = train_linear_unit()
+    print(linear_unit)
+    print('Work 3.4 years, monthly salary = %.2f' % linear_unit.predict([3.4]))
+    print('Work 15 years, monthly salary = %.2f' % linear_unit.predict([15]))
+    print('Work 1.5 years, monthly salary = %.2f' % linear_unit.predict([1.5]))
+    print('Work 6.3 years, monthly salary = %.2f' % linear_unit.predict([6.3]))
+    plot(linear_unit)
