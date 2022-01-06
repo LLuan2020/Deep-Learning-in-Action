4914c2940f29f4efa253952f9c1b605831ffea01
Minerva-Lan <lan2011gator@gmail.com>
2022-01-05 16:01:01 -0800

    Working on SGD

---
+++ Stochastic Gradient Descent/linear_unit.py
@@ -0,0 +1,8 @@
+from Perceptron.perceptron import Perceptron
+
+f = lambda x:x
+class LinearUnit(Perceptron):
+    def __init__(self, input_num):
+        Perceptron.__init__(self, input_num, f)
+
+
